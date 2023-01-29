e8953981505f4eefb8714e2166ff3381edffde47
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2023-01-29 01:49:10 -0600

    add external state management

--- compend/bot.py
+++ compend/bot.py
@@ -2,11 +2,10 @@
 import discord
 from discord.ext import commands
 import logging
-from views.menu import PaginatedDropdownMenu
 
 import utils.logging as lg
 from handler.settings import COMPEND_DISCORD_TOKEN
-from handler import library
+from handler import library, state_management
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -15,11 +14,12 @@ lg.attach_stdout_handler(logger)
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(case_insensitive=True, intents=intents)
-
+manager = state_management.StateManager()
 
 @bot.event
 async def on_ready() -> None:
     logger.info(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    
 
 
 @bot.slash_command(name="ping", description="Sends the bot's latency.") 
@@ -33,9 +33,9 @@ async def ping(ctx: discord.ApplicationContext): # a slash command will be creat
 )
 async def home_page(ctx: discord.ApplicationContext, target_url: str = "") -> None:
     if not target_url: 
-        await library.get_home_title(ctx)
+        await library.get_home_title(ctx, manager)
         return
-    await library.target_note_home(ctx, target_url)
+    await library.target_note_home(ctx, manager, target_url)
 
 
 @bot.slash_command(
@@ -43,13 +43,13 @@ async def home_page(ctx: discord.ApplicationContext, target_url: str = "") -> No
     description="Pick a session note page.  Requires the home page to be set.",
 )
 async def session_page(ctx: discord.ApplicationContext) -> None:
-    await library.target_session_page(ctx)
+    await library.target_session_page(ctx, manager)
     
 @bot.slash_command(name="current_session",
                    description="Get info from current session.  /current_session help to list available options"
 )
 async def current_session(ctx: discord.ApplicationContext, property: str) -> None:
-    await library.get_session_title(ctx)    
+    await library.get_session_title(ctx, manager)    
 
 
 
--- compend/handler/library.py
+++ compend/handler/library.py
@@ -3,10 +3,11 @@ import asyncio
 from strenum import StrEnum
 import logging
 from typing import List
-from discord import ApplicationContext, SelectOption, Interaction
+from discord import ApplicationContext, SelectOption, Interaction, Bot
 
 from notion.data_types import PageObject, BlockType
 import notion.api as notion
+from handler.state_management import StateManager
 from views.menu import PaginatedDropdownMenu
 import utils.logging as lg
 
@@ -15,11 +16,8 @@ logger.setLevel(logging.INFO)
 lg.attach_stdout_handler(logger)
 
 
-NOTE_HOME_PAGE: PageObject = None
-NOTE_SESSION_PAGE: PageObject = None
-
-
 MessageType = StrEnum("MessageType", ["FAIL", "SUCCESS", "INFO"])
+
 
 async def emoji_reply(ctx: ApplicationContext, reply: str, msg_type: MessageType, additional_emojis: List[str] = None) -> None:
     """
@@ -43,38 +41,36 @@ async def emoji_reply(ctx: ApplicationContext, reply: str, msg_type: MessageType
         asyncio.gather(*[response.add_reaction(eji) for eji in additional_emojis])
         
 
-
-async def get_home_title(ctx: ApplicationContext) -> None:
+async def get_home_title(ctx: ApplicationContext, manager: StateManager) -> None:
     """
-    Attempts to retrieve the title of the NOTE_HOME_PAGE
-    TODO: This is something that should be stored in external memory
+    Attempts to retrieve the title of the note home page
     """
-    if not NOTE_HOME_PAGE:
+    
+    home_page = await manager.get_page(ctx.guild_id, "home")
+    if not home_page:
         await emoji_reply(
             ctx, "Home page is not currently specified.", MessageType.INFO
         )
         return
     await emoji_reply(
         ctx,
-        f"Home page currently set to {NOTE_HOME_PAGE.page_title}.",
+        f"Home page currently set to {home_page.page_title}.",
         MessageType.INFO,
     )
     return
 
 
-async def target_note_home(ctx: ApplicationContext, target_url: str) -> None:
+async def target_note_home(ctx: ApplicationContext, manager: StateManager, target_url: str) -> None:
     """
-    Defines the NOTE_HOME_PAGE global using the sent url if not yet defined
-    TODO: This is something that should be stored in external memory, not global vars
+    Defines the note home page using the sent url if not yet defined
     """
-    global NOTE_HOME_PAGE
     if not target_url:
-        await get_home_title(ctx)
+        await get_home_title(ctx, manager)
         return
     try:
         page_object = await notion.get_page_object_for_url(target_url)
+        manager.save_page(ctx.guild_id, "home", page_object)
         page_title = page_object.page_title
-        NOTE_HOME_PAGE = page_object
         await emoji_reply(ctx, f"Home page set to {page_title}.", MessageType.SUCCESS, ["🏠"])
     except ValueError as ve:
         await emoji_reply(ctx, str(ve), MessageType.FAIL)
@@ -84,41 +80,41 @@ async def target_note_home(ctx: ApplicationContext, target_url: str) -> None:
         await emoji_reply(ctx, str(ue), MessageType.FAIL, ["🔤"])
 
 
-async def get_session_title(ctx: ApplicationContext) -> None:
+async def get_session_title(ctx: ApplicationContext, manager: StateManager) -> None:
     """
-    Attempts to retrieve the title of the NOTE_SESSION_PAGE
-    TODO: This is something that should be stored in external memory
+    Attempts to retrieve the title of the note session page. 
     """
-    if not NOTE_SESSION_PAGE:
+    session_page = await manager.get_page(ctx.guild_id, "session")
+    if not session_page:
         await emoji_reply(ctx, "Session is not currently specified.", MessageType.INFO)
         return
     await emoji_reply(
         ctx,
-        f"Session currently set to {NOTE_SESSION_PAGE.page_title}.",
+        f"Session currently set to {session_page.page_title}.",
         MessageType.INFO,
     )
     return
 
 
-async def target_session_page(ctx: ApplicationContext) -> None:
+async def target_session_page(ctx: ApplicationContext, manager: StateManager) -> None:
     """
     Using the current note home page, creates a dropdown menu for the user to select a session page from.
-    TODO: This is something that should be stored in external memory
     """
+    home_page = await manager.get_page(ctx.guild_id, "home")
     try:
-        if not NOTE_HOME_PAGE:
+        if not home_page:
             raise ValueError(
                 "Need to specify the note page via /home <target_url> before selecting a session page."
             )
         children = await notion.get_children_by_type(
-            NOTE_HOME_PAGE.id, BlockType.child_page
+            home_page.id, BlockType.child_page
         )
         sorted_children = sorted(children, key=lambda c: c.child_page.title)
         options = [SelectOption(label = c.child_page.title, value=c.id) for c in sorted_children]
-        async def menu_callback(selections: List[SelectOption], intxn: Interaction) -> None:
-            global NOTE_SESSION_PAGE
-            NOTE_SESSION_PAGE = await notion.get_page_object_for_id(selections[0])
-            title = NOTE_SESSION_PAGE.page_title
+        async def menu_callback(selections: List[SelectOption], _intxn: Interaction) -> None:
+            session_page = await notion.get_page_object_for_id(selections[0])
+            manager.save_page(ctx.guild_id, "session", session_page)
+            title = session_page.page_title
             await emoji_reply(ctx, f"Session page set to {title}", MessageType.SUCCESS, ["📓"])
         view = PaginatedDropdownMenu(options=options, placeholder="Choose a session page", callback=menu_callback)
         await ctx.respond(view=view)
