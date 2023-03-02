c7f625658e2298b98ed215a354049d4df77cab3c
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2023-03-02 02:52:35 -0600

    additional test coverage for state management

--- compend/tests/test_handler/test_state_management.py
+++ compend/tests/test_handler/test_state_management.py
@@ -1,5 +1,5 @@
 import pytest
-
+from unittest.mock import patch
 
 from handler.state_management import UnsavedObjectException
 from handler import state_management
@@ -14,16 +14,40 @@ class TestStateManager:
     def test_save_page(self, fake_redis_client, fake_notion_page_object):
         fake_notion_page_object.id = 1234
         self.manager.save_page("0000", "test", fake_notion_page_object)
-
         expected = {"0000": {"test": fake_notion_page_object}}
-
         assert expected.items() <= self.manager.states.items()
         fake_redis_client.set.assert_called_once_with("0000:test", 1234)
-
-    def test_get_from_state(self, fake_notion_page_object):
+        
+    def test_get_from_state_unsaved(self):
+        with pytest.raises(UnsavedObjectException):
+            _obj = self.manager.get_from_state("1111", "foo")        
+    
+    def test_get_from_state_success(self, fake_notion_page_object):
         fake_notion_page_object.id = 1337
         self.manager.save_page("0000", "foo", fake_notion_page_object)
         obj = self.manager.get_from_state("0000", "foo")
         assert obj == fake_notion_page_object
-        with pytest.raises(UnsavedObjectException):
-            _obj = self.manager.get_from_state("1111", "foo")
+        
+    async def test_get_page_unsaved_page(self, fake_redis_client):
+        fake_redis_client.get.return_value = None
+        obj = await self.manager.get_page("0000", "foo")
+        assert obj is None
+        fake_redis_client.get.assert_called_once_with("0000:foo")
+
+  
+    @patch("handler.state_management.StateManager.save_page")    
+    @patch("handler.state_management.get_page_object_for_id")
+    async def test_get_page_unsaved_object(self, mock_api_get_page, mock_save_page, fake_notion_page_object, fake_redis_client):
+        fake_redis_client.get.return_value = 1000
+        mock_api_get_page.return_value = fake_notion_page_object
+        obj = await self.manager.get_page("0000", "foo")
+        
+        assert obj == fake_notion_page_object
+        fake_redis_client.get.assert_called_once_with("0000:foo")
+        mock_save_page.assert_called_once_with("0000", "foo", fake_notion_page_object)
+        
+        
+        
+        
+        
+    
